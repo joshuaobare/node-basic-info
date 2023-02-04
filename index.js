bf57aab8079e122ed063774a313a2d21bf422f62
joshuaobare <jobare.obare@gmail.com>
2023-02-04 16:23:40 +0300

    added check so the home url loads index.html

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const http = require('http');
-const fs = require("fs")
-const url = require("url")
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const hostname =  "127.0.0.1"
-const port = 8080
+const hostname = "127.0.0.1";
+const port = 8080;
 
-http.createServer((req,res) => {
-    const q = url.parse(req.url, true)
-    const filename = "." + q.pathname
-    fs.readFile(filename, (err,data) => {
-        if(err) {
-            fs.readFile("404.html" , (error, errorData) => {
-                if(error){
-                    res.writeHead(500 , {"Content-Type": "text/html"} )
-                    return res.end("Internal Server Error")
-                }
-                res.writeHead(404 ,{"Content-Type": "text/html"} )
-                res.write(errorData)
-                return res.end()
-            })
-            
-        }
-        
-        else {
-            res.writeHead(200 , {"Content-Type": "text/html"})
-        res.write(data)
-        return res.end()
-        }
-        
-        
-    })
-    
-}).listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`)
-})
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const filename = "." + q.pathname;
+
+    if (req.url === "/") {
+      fs.readFile("index.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        fs.readFile("404.html", (error, errorData) => {
+          if (error) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            return res.end("Internal Server Error");
+          }
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(errorData);
+          return res.end();
+        });
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
+    });
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`);
+  });
